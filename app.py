f94af4f7b07e7823c7b795a05bccb7b122c3c31d
Brendan Weibel <me@bwhybel.com>
2023-01-21 09:13:57 -0600

    adding more

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
 
@@ -10,19 +9,19 @@ def index():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+   return send_from_directory(os.path.join(app.root_path, 'static'),
+                              'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
 
    if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+      print('Request for hello page received with name=%s' % name)
+      return render_template('hello.html', name = name)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+      print('Request for hello page received with no name or blank name -- redirecting')
+      return redirect(url_for('index'))
 
 
 if __name__ == '__main__':

248f8a37d65228fe903bad2265c0e29a41971ec4
Brendan Weibel <me@bwhybel.com>
2023-12-29 21:41:26 -0600

    Finish the migration

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from flask import (
 )
 
 from workouts import workouts_blueprint
+from teams import teams_blueprint
+from groups import groups_blueprint
+from auth import auth_blueprint
 import datetime
 import os
 
@@ -16,6 +19,9 @@ from extensions import get_container, get_permission_and_teams, get_teams, rando
 
 app = Flask(__name__)
 app.register_blueprint(workouts_blueprint, url_prefix="/workouts")
+app.register_blueprint(teams_blueprint, url_prefix="/teams")
+app.register_blueprint(groups_blueprint, url_prefix="/groups")
+app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
 
 @app.route("/")
@@ -52,141 +58,6 @@ def index():
         should_create_new_teams=permission == "rwe",
         should_create_new_groups=permission == "rw" or permission == "rwe",
     )
-
-
-@app.route("/team")
-def write_team():
-    cookie = request.cookies.get("workouts_permissions")
-    permission, _ = get_permission_and_teams(cookie)
-
-    if permission != "rwe":
-        return redirect(url_for("index"), code=302)
-
-    return render_template("team_writer.html")
-
-
-@app.route("/team/<id>")
-def write_team_id(id):
-    cookie = request.cookies.get("workouts_permissions")
-    permission, _ = get_permission_and_teams(cookie)
-
-    if permission != "rwe":
-        return redirect(url_for("index"), code=302)
-
-    teams = get_container("teams").query_items(
-        query=f"SELECT * FROM teams t WHERE t.id = '{id}'",
-        enable_cross_partition_query=True,
-    )
-
-    real_team = {}
-    for team in teams:
-        real_team = team
-
-    return render_template(
-        "team_writer.html",
-        id=real_team["id"],
-        name=real_team["name"],
-        code=real_team["code"],
-        image=real_team["image"],
-        logo_width=real_team["logo_width"],
-    )
-
-
-@app.route("/team", methods=["POST"])
-def team():
-    id = request.form.get("id")
-    if not id:
-        id = random_id()
-    name = request.form.get("name")
-    code = request.form.get("code")
-    image = request.form.get("image")
-    logo_width = request.form.get("logo_width")
-    get_container("teams").upsert_item(
-        {"id": id, "name": name, "code": code, "image": image, "logo_width": logo_width}
-    )
-    return redirect(url_for("write_team_id", id=id), code=301)
-
-
-@app.route("/code", methods=["POST"])
-def code():
-    expiration_date = datetime.datetime.now() + datetime.timedelta(days=30)
-    code = request.form.get("code")
-
-    codes = list(
-        get_container("code_to_permission").query_items(
-            query=f"SELECT * FROM code_to_permission c WHERE c.id = '{code}'",
-            enable_cross_partition_query=True,
-        )
-    )
-
-    if len(codes) == 0:
-        return redirect(url_for("index"), code=301)
-
-    cookie_value = codes[0]["cookie"]
-
-    response = make_response(redirect(url_for("index"), code=301))
-    response.set_cookie(
-        "workouts_permissions", value=cookie_value, expires=expiration_date
-    )
-    return response
-
-
-@app.route("/group")
-def write_group():
-    cookie = request.cookies.get("workouts_permissions")
-    permission, _ = get_permission_and_teams(cookie)
-
-    if permission not in ["rw", "rwe"]:
-        return redirect(url_for("index"), code=302)
-
-    return render_template("group_writer.html", teams=get_teams())
-
-
-@app.route("/group/<id>")
-def write_group_id(id):
-    cookie = request.cookies.get("workouts_permissions")
-    permission, _ = get_permission_and_teams(cookie)
-
-    if permission not in ["rw", "rwe"]:
-        return redirect(url_for("index"), code=302)
-
-    groups = get_container("groups").query_items(
-        query=f"SELECT * FROM groups g WHERE g.id = '{id}'",
-        enable_cross_partition_query=True,
-    )
-
-    real_group = {}
-    for group in groups:
-        real_group = group
-
-    return render_template(
-        "group_writer.html",
-        id=real_group["id"],
-        name=real_group["name"],
-        code=real_group["code"],
-        teams=get_teams(),
-        team_id=real_group["team_id"],
-    )
-
-
-@app.route("/group", methods=["POST"])
-def group():
-    id = request.form.get("id")
-    if not id:
-        id = random_id()
-    name = request.form.get("name")
-    code = request.form.get("code")
-    team = request.form.get("team")
-
-    cookie = request.cookies.get("workouts_permissions")
-    permission, team_ids = get_permission_and_teams(cookie)
-    if (permission != "rw" and permission != "rwe") or team not in team_ids:
-        return redirect(url_for("index"), code=301)
-
-    get_container("groups").upsert_item(
-        {"id": id, "name": name, "code": code, "team_id": team}
-    )
-    return redirect(url_for("write_group_id", id=id), code=301)
 
 
 @app.route("/favicon.ico")
